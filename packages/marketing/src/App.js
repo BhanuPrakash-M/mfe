c03192bf07500221d6b1f75e9df3f44a8a3be75c
BhanuPrakash-M <bhanu.mallepoola@publicissapient.com>
2023-01-19 12:25:42 +0530

    Added some style collide changes

--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,14 +1,19 @@
 import React from "react";
 import {Switch, Route, BrowserRouter} from "react-router-dom";
-import { styleProvider } from "@material-ui/core/styles";
+import { styleProvider, createGenerateClassName } from "@material-ui/core/styles";
 
 import Landing from "./components/Landing";
 import Pricing from "./components/Pricing";
 
+
+const generateClassName = createGenerateClassName({
+    productionPrefix: 'ma'
+});
+
 export default () => {
     return (
         <div>
-            <styleProvider>
+            <styleProvider generateClassName={generateClassName}>
                 <BrowserRouter>
                  <Switch>
                     <Route exact path="/pricing" component={Pricing} />
